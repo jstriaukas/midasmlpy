03253f545071f7a2b90c931e88a49ef52bfdd911
jstriaukas <jonas.striaukas@gmail.com>
2022-03-14 15:19:50 +0200

    update main functions

--- midasmlpy/date_functions.py
+++ midasmlpy/date_functions.py
@@ -1,8 +1,5 @@
 # 3-11 work content
-import statistics
 from datetime import datetime
-# ISO Date Format: 2021-07-07 06:36:55
-# Formatted Time: 23/06/21 10:33:02
 from unicodedata import numeric
 
 #time input style: [year,month,day,hour,min,sec]
@@ -78,7 +75,8 @@ def lag_num(x_lag, period, unit):
 
 def mode_midasml(data):
     nobs = len(data)
-    data = nobs.sort()
+    data = data.tolist()
+    data.sort()
     count = 1
     countMax = 1
     dataMode = data[0]
@@ -98,16 +96,32 @@ def mode_midasml(data):
         "countMax": countMax
     }
     
-
+import statistics
+import numpy as np
 
 def data_freq(DateVec):
     ini_DV = DateVec
-    for row in range(len(DateVec)[1] - 1):
-        for col in range(len(DateVec)[0]):
-            DateVec[col][row] = DateVec[col + 1][row] - DateVec[col][row]
+    DateVec = np.array(DateVec)
+    shape_vec = DateVec.shape
+    for i in range(len(DateVec) - 1):
+        DateVec[i]=DateVec[i + 1] - DateVec[i]
+        total_days = DateVec[i][0] * 360 + DateVec[i][1] * 30 + DateVec[i][2]
+        if total_days >= 0:
+            if DateVec[i][2] < 0:
+                DateVec[i][2] += 30
+                DateVec[i][1] -= 1
+                if DateVec[i][1] < 0:
+                    DateVec[i][1] += 12
+                    DateVec[i][0] -=1
+            elif DateVec[i][1] < 0:
+                DateVec[i][1] += 12
+                DateVec[i][0] -=1
             
+    DateVec = np.array(DateVec[:-1])
+    y = DateVec.shape
+    x= DateVec[:,0]
     # Check annual or lower frequency
-    modeUse = mode_midasml(DateVec[:][0])
+    modeUse = mode_midasml(DateVec[:,0])
     modeUse = modeUse["dataMode"]
     if modeUse >= 1:
         period = modeUse
@@ -118,7 +132,7 @@ def data_freq(DateVec):
         }
         
     # Check monthly frequency, quarter = 3 months, semiannual = 6 months
-    modeUse = mode_midasml(DateVec[:][1])
+    modeUse = mode_midasml(DateVec[:,1])
     modeUse = modeUse["dataMode"]
     if modeUse < 0:
         modeUse = modeUse + 12
@@ -131,7 +145,7 @@ def data_freq(DateVec):
         }
         
     # Check daily frequency, week = 7 days, biweekly = 14 days
-    modeUse = mode_midasml(DateVec[:][2])
+    modeUse = mode_midasml(DateVec[:,2])
     modeUse = modeUse["dataMode"]
     if modeUse < 0:
         modeUse += 30
@@ -144,7 +158,7 @@ def data_freq(DateVec):
         }
     
     # Check hourly frequency
-    modeUse = mode_midasml(DateVec[:][3])
+    modeUse = mode_midasml(DateVec[:,3])
     modeUse = modeUse["dataMode"]
     if modeUse < 0:
         modeUse += 24
@@ -157,7 +171,7 @@ def data_freq(DateVec):
         }
         
   # Check minutely frequency
-    modeUse = mode_midasml(DateVec[:][4])
+    modeUse = mode_midasml(DateVec[:,4])
     modeUse = modeUse["dataMode"]
     if modeUse < 0:
         modeUse += 60
@@ -170,7 +184,7 @@ def data_freq(DateVec):
         }
   
   # Check secondly frequency  
-    elapse = diff_time_mf(ini_DV[1:][5], ini_DV[0:][5], origin = "1970-01-01", units = "secs")
+    elapse = diff_time_mf(ini_DV[:,5], ini_DV[:,5], origin = "1970-01-01", units = "secs")
     period = statistics.mean(elapse)
     unit = 6
     return {
@@ -201,20 +215,23 @@ def dateMatch(x, y):
                 x_out[i] = i_x
     return x_out
 
-
+from datetime import datetime
+# ISO Date Format: 2021-07-07 06:36:55
+# Formatted Time: 23/06/21 10:33:02
 def corr_datetype(date):
-    date = "2021-07-07 06:36:55"
-    x = date.split(" ")
-    x_len = len(x)
-    if x_len == 1:
-        return date + " 00:00:00"
-    else:
+    for i in range(len(date)):
+        x = date[i].split(" ")
+        x_len = len(x)
+        if x_len == 1:
+            date[i] += " 00:00:00"
+        x = date[i].split(" ")
         if "/" in x[0]:
             x_conv = x[0].split("/")
-            year = str(int(x_conv) + 2000)
-            month = x_conv[1]
-            day = x_conv[0]
-            return year + "-" + month + "-" + day + " " + x[1]
+            year = x_conv[2]
+            month = x_conv[0]
+            day = x_conv[1]
+            date[i] = year + "-" + month + "-" + day + " " + x[1]
+    return date
 
 def monthBegin(x):
     x = corr_datetype(x)
@@ -235,9 +252,9 @@ def monthEnd(x):
 
 def is_na(data):
     data_temp = []
-    for i, y in numeric(data):
-        if data[i] is not None:
-            data_temp.append(data[i])
+    for i, y in enumerate(data):
+        if y is not None:
+            data_temp.append(y)
         else: continue
     return data_temp
     
@@ -246,14 +263,12 @@ def date_vec(s):
     h = len(s)
     mat = [[0 for x in range(w)] for y in range(h)]
     for i in range(h): 
-        date = corr_datetype(s[i])
-        df = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-        mat[i][0] = df.year
-        mat[i][1] = df.month
-        mat[i][2] = df.day
-        mat[i][3] = df.hour
-        mat[i][4] = df.minute
-        mat[i][5] = df.second
+        mat[i][0] = s[i].year
+        mat[i][1] = s[i].month
+        mat[i][2] = s[i].day
+        mat[i][3] = s[i].hour
+        mat[i][4] = s[i].minute
+        mat[i][5] = s[i].second
     return mat
 
 def mixed_freq_data(data_y, data_ydate, data_x, data_xdate, x_lag, y_lag, horizon, est_start, est_end, disp_flag = True):
@@ -291,8 +306,8 @@ def mixed_freq_data(data_y, data_ydate, data_x, data_xdate, x_lag, y_lag, horizo
         min_date = min_date_y
     else:
         min_date = min_date_x
-    max_date_y = data_ydate_num[len(data_ydate_num)]
-    max_date_x = data_xdate_num[len(data_xdate_num)]
+    max_date_y = data_ydate_num[len(data_ydate_num) - 1]
+    max_date_x = data_xdate_num[len(data_xdate_num) - 1]
     if horizon < 0:
         max_date_x = data_xdate_vec[len(data_xdate_vec)[0]][:]
         max_date_x[unit_x] = max_date_x[unit_x] + period_x * horizon
@@ -315,17 +330,17 @@ def mixed_freq_data(data_y, data_ydate, data_x, data_xdate, x_lag, y_lag, horizo
             print("Terminal date cannot be later than largest date account for lags. Reset to largest date.")
             est_end = max_date
     tol = 1e-10
-    for i in range(data_ydate_num):
+    for i in range(len(data_ydate_num)):
         if data_ydate_num[i] >= est_start - tol:
             loc_start = i
             break
-    for i in range(data_ydate_num):
+    for i in range(len(data_ydate_num)):
         if data_ydate_num[i] >= est_end - tol:
             loc_end = i
             break
     est_y = data_y[loc_start : loc_end]
     est_ydate = data_ydate_num[loc_start : loc_end]
-    for i in range(data_ydate_num):
+    for i in range(len(data_ydate_num)):
         if data_ydate_num[i] >= max_date - tol:
             loc_forecast_end = i
             break
@@ -337,18 +352,21 @@ def mixed_freq_data(data_y, data_ydate, data_x, data_xdate, x_lag, y_lag, horizo
         out_y, out_ydate = None
         n_forecast = 0
     nobs = loc_end - loc_start + 1
-    est_lag_y, est_lag_ydate = [[None for x in range(y_lag)] for y in range(nobs)]
+    est_lag_y = [[None for x in range(y_lag)] for y in range(nobs)]
+    est_lag_ydate = [[None for x in range(y_lag)] for y in range(nobs)]
     for m in range(y_lag):
         est_lag_y[:][m] = data_y[loc_start - m - 1, loc_end - m]
         est_lag_ydate[:][m] = data_ydate_num[loc_start - m - 1, loc_end - m]
     if loc_end + 1 <= loc_forecast_end:
-        out_lag_y, out_lag_ydate = [[None for x in range(y_lag)] for y in range(n_forecast)]
+        out_lag_y = [[None for x in range(y_lag)] for y in range(n_forecast)]
+        out_lag_ydate = [[None for x in range(y_lag)] for y in range(n_forecast)]
         for m in range(y_lag):
             out_lag_y[:][m] = data_y[loc_end - m, loc_forecast_end - m - 1]
             out_lag_ydate[:][m] = data_ydate_num[loc_end - m, loc_forecast_end - m - 1]
     else:
         out_lag_y, out_lag_ydate = None
-    est_x, est_xdate = [[None for x in range(y_lag)] for y in range(nobs)]
+    est_x = [[None for x in range(y_lag)] for y in range(nobs)]
+    est_xdate = [[None for x in range(y_lag)] for y in range(nobs)]
     for t in range(nobs):
         for i in range(len(data_xdate_num)):
             if data_xdate_num[i] >= est_ydate[t] - tol:
@@ -371,7 +389,8 @@ def mixed_freq_data(data_y, data_ydate, data_x, data_xdate, x_lag, y_lag, horizo
             est_x[t][:] = data_x[loc - horizon - 1 : loc -horizon - x_lag : -1]
             est_xdate[t][:] = data_xdate_num[loc - horizon - 1 : loc -horizon - x_lag : -1]
     if loc_end + 1 <= loc_forecast_end:
-        out_x, outxdate = [[None for x in range(x_lag)] for y in range(n_forecast)]
+        out_x = [[None for x in range(x_lag)] for y in range(n_forecast)]
+        outxdate = [[None for x in range(x_lag)] for y in range(n_forecast)]
         for t in range(n_forecast):
             for i in range(len(data_xdate_num)):
                 if data_xdate_num[i] >= out_ydate[t] - tol:
@@ -437,15 +456,17 @@ def mixed_freq_data_single(data_refdate, data_x, data_xdate, x_lag, horizon, est
     data_x = is_na(data_x)
     data_xdate = is_na(data_xdate)
     data_refdate = corr_datetype(data_refdate)
-    data_x = corr_datetype(data_x)
     data_xdate = corr_datetype(data_xdate)
-    data_refdate = datetime.strptime(data_refdate, '%Y-%m-%d %H:%M:%S')
-    data_x = datetime.strptime(data_x, '%Y-%m-%d %H:%M:%S')
-    data_xdate = datetime.strptime(data_xdate, '%Y-%m-%d %H:%M:%S')
+    est_start = corr_datetype(est_start)
+    est_end = corr_datetype(est_end)
+    for i in range(len(data_refdate)):
+        data_refdate[i] = datetime.strptime(data_refdate[i], '%Y-%m-%d %H:%M:%S')
+    for i in range(len(data_xdate)):
+        data_xdate[i] = datetime.strptime(data_xdate[i], '%Y-%m-%d %H:%M:%S')
     data_refdate_vec = data_refdate
     data_xdate_vec = data_xdate
-    est_start = datetime.strptime(est_start, '%Y-%m-%d %H:%M:%S')
-    est_end = datetime.strptime(est_end, '%Y-%m-%d %H:%M:%S')
+    est_start = datetime.strptime(est_start[0], '%Y-%m-%d %H:%M:%S')
+    est_end = datetime.strptime(est_end[0], '%Y-%m-%d %H:%M:%S')
     data_refdate_vec = date_vec(data_refdate_vec)
     data_xdate_vec = date_vec(data_xdate_vec)
     data_refdate_num = data_refdate
@@ -473,10 +494,10 @@ def mixed_freq_data_single(data_refdate, data_x, data_xdate, x_lag, horizon, est
         min_date = min_date_ref
     else:
         min_date = min_date_x
-    max_date_ref = data_refdate_num[len(data_refdate_num)]
-    max_date_x = data_xdate_num[len(data_xdate_num)]
+    max_date_ref = data_refdate_num[len(data_refdate_num) - 1]
+    max_date_x = data_xdate_num[len(data_xdate_num) - 1]
     if horizon < 0:
-        max_date_x = data_xdate_vec[len(data_xdate_vec)[0]][:]
+        max_date_x = data_xdate_vec[len(data_xdate_vec)[0] - 1][:]
         max_date_x[unit_x] = max_date_x[unit_x] + period_x * horizon
         max_date_x = str(max_date_x[0]) + "-" +str(max_date_x[1]) + "-" +str(max_date_x[2]) + " " +str(max_date_x[3]) + ":" +str(max_date_x[4]) + ":" +str(max_date_x[5])
         max_date_x = datetime.strptime(max_date_x, '%Y-%m-%d %H:%M:%S')
@@ -498,18 +519,20 @@ def mixed_freq_data_single(data_refdate, data_x, data_xdate, x_lag, horizon, est
             est_end = max_date
     # Construct reference date data
     tol = 1e-10
-    for i in range(data_refdate_num):
-        if data_refdate_num[i] >= est_start - tol:
+    loc_start = 0
+    for i in range(len(data_refdate_num)):
+        
+        if data_refdate_num[i] >= est_start:
             loc_start = i
             break
-    for i in range(data_refdate_num):
-        if data_refdate_num[i] >= est_end - tol:
+    for i in range(len(data_refdate_num)):
+        if data_refdate_num[i] >= est_end:
             loc_end = i
             break
     est_refdate = data_refdate_num[loc_start:loc_end]
     
-    for i in range(data_refdate_num):
-        if data_refdate_num[i] >= max_date - tol:
+    for i in range(len(data_refdate_num)):
+        if data_refdate_num[i] >= max_date:
             loc_forecast_end = i
             break
     if loc_end + 1 <= loc_forecast_end:
@@ -518,29 +541,35 @@ def mixed_freq_data_single(data_refdate, data_x, data_xdate, x_lag, horizon, est
     else:
         out_refdate = None
         n_forecast = len(out_refdate)
-    nobs = loc_end - loc_start + 1
-    est_x, est_xdate = [[None for x in range(x_lag)] for y in range(n_forecast)]
-    for t in range(n_forecast):        
-        for i in range(data_xdate_num):
-            if data_xdate_num[i] >= out_refdate[t] - tol:
+    nobs = loc_end - loc_start
+    est_x = [[None for x in range(x_lag)] for y in range(nobs)]
+    est_xdate = [[None for x in range(x_lag)] for y in range(nobs)]
+    for t in range(nobs):
+        for i in range(len(data_xdate_num)):
+            x = data_xdate_num[i]
+            y = est_refdate[t]
+            if x >= y:
                 loc = i
                 break
         if loc is None:
             loc = len(data_xdate_num)
         if loc - horizon > len(data_x):
-            n_forecast = t - 1
-            out_refdate = out_refdate[:n_forecast]
-            out_x = out_x[:n_forecast]
-            out_xdate = out_xdate[:n_forecast]
+            nobs = t - 1
+            est_refdate = est_refdate[:nobs]
+            est_x = est_x[:nobs]
+            est_xdate = est_xdate[:nobs]
+            max_date = est_refdate[len(est_refdate)]
+            print("Horizon is a large negative number. Observations are further truncated to max date possible", max_date)
             break
         else:
-            out_x[t][:] = data_x[loc - horizon - 1 : loc - horizon - x_lag : -1]
-            out_xdate = data_xdate_num[loc - horizon - 1 : loc - horizon - x_lag : -1]
+            est_x[t][:] = data_x[loc - horizon - 1 : loc -horizon - x_lag : -1]
+            est_xdate[t][:] = data_xdate_num[loc - horizon - 1 : loc -horizon - x_lag : -1]
     if loc_end + 1 <= loc_forecast_end:
-        out_x, out_xdate = [[None for x in range(x_lag)] for y in range(n_forecast)]
+        out_x = [[None for x in range(x_lag)] for y in range(n_forecast)]
+        out_xdate = [[None for x in range(x_lag)] for y in range(n_forecast)]
         for t in range(n_forecast):            
-            for i in range(data_xdate_num):
-                if data_xdate_num[i] >= out_refdate[t] - tol:
+            for i in range(len(data_xdate_num)):
+                if data_xdate_num[i] >= out_refdate[t]:
                     loc = i
                     break
             if loc is None:
@@ -569,11 +598,11 @@ def mixed_freq_data_single(data_refdate, data_x, data_xdate, x_lag, horizon, est
             if x_lag == 1:
                 print("X(", est_xdate[m][0], ")`s")
             if x_lag == 2:
-                print("X(", est_xdate[m][0], ")`s X(", est_xdate[m][len(est_xdate)[1]], ")`s")
+                print("X(", est_xdate[m][0], ")`s X(", est_xdate[m][10], ")`s ...")
             if x_lag == 3:
-                print("X(", est_xdate[m][0],")`s Y(", est_xdate[m][1],")`s X(", est_xdate[m][len(est_xdate)[1]], ")`s")
+                print("X(", est_xdate[m][0],")`s Y(", est_xdate[m][1],")`s X(", est_xdate[m][10], ")`s ...")
             if x_lag > 3:
-                print("X(", est_xdate[m][0],")`s Y(", est_xdate[m][1],")`s ... X(", est_xdate[m][len(est_xdate)[1]], ")`s")
+                print("X(", est_xdate[m][0],")`s Y(", est_xdate[m][1],")`s ... X(", est_xdate[m][10], ")`s ...")
            
     return {
         "est_refdate": est_refdate,
@@ -586,4 +615,17 @@ def mixed_freq_data_single(data_refdate, data_x, data_xdate, x_lag, horizon, est
         "min_date": min_date,
         "max_date": max_date,
     }
-        
+
+if __name__ == "__main__":
+    with open("input_data_refdate.txt") as file_data_refdate:
+        data_refdate = [line.rstrip() for line in file_data_refdate]
+    with open("input_data_x.txt") as file_data_x:
+        data_x = [line.rstrip() for line in file_data_x]
+    with open("input_data_xdate.txt") as file_data_xdate:
+        data_xdate = [line.rstrip() for line in file_data_xdate]
+    x_lag = 12
+    horizon = 1
+    est_start = ["1990-01-01"]
+    est_end = ["2002-03-01"]
+    result = mixed_freq_data_single(data_refdate, data_x, data_xdate, x_lag, horizon, est_start, est_end, disp_flag = True)
+    print(result)
